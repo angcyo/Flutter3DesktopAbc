5073465fc6fcb7f504ce4a5d3d463b4852f5f1be
angcyo <angcyo@126.com>
2025-02-12 22:45:37 +0800

    + windows bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,7 @@
 @echo off
 rem 设置当前控制台为UTF-8编码
 chcp 65001 >> nul
+
+rem Building Windows application...                                   165.0s
+rem √ Built build\windows\x64\runner\Release\flutter3_desktop_abc_bn.exe
 dart run Flutter3Core/script/build_config.dart & flutter build windows --release
