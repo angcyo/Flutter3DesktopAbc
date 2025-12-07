c05fe455d6ef0102454b4ec14a6324e051847e9b
angcyo <angcyo@126.com>
2025-12-07 09:20:02 +0800

    * fix base app

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,20 @@ import 'src/desktop_app.dart';
 void main([List<String>? args]) async {
   //await initWindow(size: Size(1280, 720));
   //runApp(const DesktopApp());
-  runGlobalApp(const DesktopApp(), beforeAction: () async {
-    /*GlobalConfig.def.openUrlFn = (context, url, meta) {
+  runGlobalApp(
+    const DesktopApp().wrapAboutDialog(),
+    beforeAction: () async {
+      /*GlobalConfig.def.openUrlFn = (context, url, meta) {
       context?.openSingleWebView(url);
       return Future.value(true);
     };*/
 
-    await initDesktopApp();
-    await initWindow(size: Size(1280, 720));
-    //lp
-    await initLpModule();
-    //--
-    SvgBuilder.customSvgHeaderAnnotation = null;
-  });
+      await initDesktopApp();
+      await initWindow(size: Size(1280, 720));
+      //lp
+      await initLpModule();
+      //--
+      SvgBuilder.customSvgHeaderAnnotation = null;
+    },
+  );
 }
