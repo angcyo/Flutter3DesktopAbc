338abbd1b2758c7486c89c16eda6fd8931cdbd95
angcyo <angcyo@126.com>
2026-08-24 11:15:29 +0800

    * init

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import app_links
 import audio_decoder
 import device_info_plus
-import file_picker
+import file_picker_darwin
 import file_selector_macos
 import flutter_angle
 import flutter_blue_plus_darwin
